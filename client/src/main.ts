97df3000799da2675d65d0d2fe47a5ed825c51bd
Robert Bossaert <contact@robertbossaert.com>
2025-06-12 14:17:44 +0200

    (chore): remove pinia

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,14 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createVfm } from 'vue-final-modal'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(createVfm())
 app.use(router)
 
